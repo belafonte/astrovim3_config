dc57f42b700a8900bfc4dde118560b8062972585
Jan Pistor <pistor.jan@gmail.com>
2023-09-13 11:08:20 +0200

    refactor: highlights

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,24 @@
+local comment_highlight = vim.api.nvim_get_hl_by_name("Comment", true)
+local comment_fg_color = comment_highlight.foreground
+local comment_bg_color = comment_highlight.background
+
+local normal_highlight = vim.api.nvim_get_hl_by_name("Normal", true)
+local normal_fg_color = comment_highlight.foreground
+local normal_bg_color = comment_highlight.background
+
 return { -- this table overrides highlights in all themes
-  -- Normal = { bg = "#000000" },
+  -- Normal = { bg = comment_fg_color },
+  BufferLineFill = { bg = "bg" },
+  BufferLineOffsetSeparator = { fg = comment_bg_color, bg = "bg" },
+  NeoTreeNormal = { bg = "bg" },
+  NeoTreeEndOfBuffer = { bg = "bg" },
+  NormalFloat = { bg = "bg" },
+
+  NeotreeTabActive = { bg = "bg" },
+  NeotreeTabInactive = { bg = "bg" },
+  NeoTreeTabSeparatorInactive = { fg = "bg", bg = "bg" },
+  NeoTreeTabSeparatorActive = { fg = "bg", bg = "bg" },
+
+  -- NeoTreeNormal = {}guibg=NONE ctermbg=NONE
+  -- NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
 }
