3ab23bed4421328c6baabbca0cdf69d58369fa09
Jan Pistor <pistor.jan@gmail.com>
2023-09-14 09:13:28 +0200

    refactor: add b key mapping

--- mappings.lua
+++ mappings.lua
@@ -25,7 +25,7 @@ return {
     -- ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = "Sort by tabs" },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
+    -- ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
     -- resize windoes with meta
