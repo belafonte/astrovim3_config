871020aa3a9cdc5a625b3f3c7dd4d86768fa4b76
Jan Pistor <pistor.jan@gmail.com>
2023-11-07 11:09:19 +0100

    feat: add open tmux session with auto name from dir

--- plugins/toggleterm.lua
+++ plugins/toggleterm.lua
@@ -1,4 +1,23 @@
+local Terminal = require("toggleterm.terminal").Terminal
+local git = require "astronvim.utils.git"
+local name = git.cmd({ "rev-parse", "--show-toplevel" }, ...)
+
+local cmd = string.format("tmux new -A -s %s", name)
+-- local cmd = "tmux new -A s session-name"
+
+local tmux = Terminal:new { cmd = cmd, direction = "float" }
+
+function _tmux_toggle() tmux:toggle() end
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>tm",
+  "<cmd>lua _tmux_toggle()<CR>",
+  { noremap = true, silent = true, desc = "Open tmux" }
+)
+
 return {
+
   {
     "akinsho/toggleterm.nvim",
     version = "*",
