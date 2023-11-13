cb7b79314053478626e2d6181916cdbd66f0ab48
Jan Pistor <pistor.jan@gmail.com>
2023-11-13 09:48:37 +0100

    fix: alpha indent

--- plugins/alpha.lua
+++ plugins/alpha.lua
@@ -6,8 +6,8 @@ return {
     dashboard.section.buttons.val = {
       button("LDR n", "  New File  "),
       button("LDR f f", "  Find File  "),
-      button("LDR f o", "󰈙 Recents  "),
-      button("LDR f w", "󰈭 Find Word  "),
+      button("LDR f o", "󰈙  Recents  "),
+      button("LDR f w", "󰈭  Find Word  "),
       button("LDR f '", "  Bookmarks  "),
       button("LDR S f", "  Sessions  "),
       button("LDR fp", "  Projects  "),
