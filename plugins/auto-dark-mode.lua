9cf6d86ba022883d3d7f2d052a6006c4d0939258
Jan Pistor <pistor.jan@gmail.com>
2024-01-03 16:30:45 +0100

    refactor: trying to switch themes ...

--- plugins/auto-dark-mode.lua
+++ plugins/auto-dark-mode.lua
@@ -1,17 +1,51 @@
+-- local home = vim.fn.expand "$HOME"
+-- local themes_folder = "/Users/jpistor/.config/kitty/themes"
+--
+--
+local function setKittyColors(theme)
+  local command = "/opt/homebrew/bin/kitty @ set-colors -a -c " .. theme
+  vim.fn.system(command)
+end
 return {
   "f-person/auto-dark-mode.nvim",
   opts = {
     update_interval = 1000,
     set_dark_mode = function()
       vim.api.nvim_set_option("background", "dark")
+
+      -- local command = "/opt/homebrew/bin/kitty @ set-colors -a -c "
+      --   .. vim.fn.expand "$HOME"
+      --   .. "/.config/kitty/themes/everforest_dark_medium.conf"
+
+      -- vim.api.nvim_exec("! " .. command, true)
+      -- vim.fn.system(command)
+      local dark_theme_path = vim.fn.expand "$HOME" .. "/.config/kitty/themes/everforest_dark_medium.conf"
+      setKittyColors(dark_theme_path)
+      -- local command =
+      --   "/opt/homebrew/bin/kitty @ set-colors -a -c /Users/jpistor/.config/kitty/themes/everforest_dark_medium.conf"
+      -- os.execute(command)
+
       -- vim.cmd.colorscheme("tokyonight-moon")
     end,
     set_light_mode = function()
       vim.api.nvim_set_option("background", "light")
-      -- vim.cmd.colorscheme("tokyonight-day")
+      local light_theme_path = vim.fn.expand "$HOME" .. "/.config/kitty/themes/everforest_light_medium.conf"
+      setKittyColors(light_theme_path)
+
+      -- local command = "/opt/homebrew/bin/kitty @ set-colors -a -c "
+      --   .. vim.fn.expand "$HOME"
+      --   .. "/.config/kitty/themes/everforest_light_medium.conf"
       --
+      -- vim.fn.system(command)
+      -- vim.api.nvim_exec("! " .. command, true)
+      -- local command =
+      --   "/opt/homebrew/bin/kitty @ set-colors -a -c /Users/jpistor/.config/kitty/themes/everforest_light_medium.conf"
+      -- os.execute(command)
+
+      -- vim.cmd.colorscheme("tokyonight-day")
     end,
   },
+
   config = function(_, opts) require("auto-dark-mode").setup(opts) end,
   init = function(_) require("auto-dark-mode").init() end,
 }
