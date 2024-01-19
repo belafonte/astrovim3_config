5e5afb7ffe3212d74e3853e2aafb7042032835b8
Jan Pistor <pistor.jan@gmail.com>
2024-01-19 10:26:23 +0100

    refactor: use gpt 4 in chat gpt

--- plugins/community.lua
+++ plugins/community.lua
@@ -75,6 +75,28 @@ return {
   { import = "astrocommunity.editing-support.nvim-regexplainer" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.editing-support.chatgpt-nvim" },
+  { -- further customize the options set by the community
+    "jackMort/ChatGPT.nvim",
+    opts = {
+      openai_params = {
+        model = "gpt-4",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 3000,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+      openai_edit_params = {
+        model = "gpt-4",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        temperature = 0,
+        top_p = 1,
+        n = 1,
+      },
+    },
+  },
 
   -- { import = "astrocommunity.editing-support.multicursors-nvim" },
 
