b5c063ec31fc354ea66dcf5be3949b3cf0218e6d
Jan Pistor <pistor.jan@gmail.com>
2024-01-19 10:26:36 +0100

    refactor: add buffer mapping back

--- mappings.lua
+++ mappings.lua
@@ -77,16 +77,16 @@ return {
     -- Save All
     ["<leader>W"] = { "<cmd>wa<cr>", desc = "Save All" },
     -- better buffer navigation
-    -- ["]b"] = false,
-    -- ["[b"] = false,
-    -- ["<S-l>"] = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- ["<S-h>"] = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+    ["]b"] = false,
+    ["[b"] = false,
+    ["<S-l>"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    ["<S-h>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
 
     -- buffer switching
     ["<Tab>"] = {
